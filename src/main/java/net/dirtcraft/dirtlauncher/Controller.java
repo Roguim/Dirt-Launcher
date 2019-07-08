23292213c2a80664477db7e64d9619a65b973302
d4julian <julianubico@icloud.com>
2019-07-08 16:38:59 -0400

    push push

--- src/main/java/net/dirtcraft/dirtlauncher/Controller.java
+++ src/main/java/net/dirtcraft/dirtlauncher/Controller.java
@@ -7,20 +7,36 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
 
 public class Controller {
 
     @FXML
     private ListView listView;
 
+    @FXML
+    private HBox topPane;
+
     boolean hasClicked = false;
 
     @FXML
-    void buttonClicked(MouseEvent event) {
+    void buttonClicked(MouseEvent event) throws IOException {
 
-        ObservableList<String> list = FXCollections.<String>observableArrayList("FTB Interactions", "FTB Revelation", "Pixelmon Reforged", "DirtCraft's DirtBOT");
         if (!hasClicked) {
+            //Main.setListView(listView);
+
+            final ObservableList<String> list = FXCollections.<String>observableArrayList(
+                    "FTB Interactions\nCreated By: FTB Team\nPlays: N/A\nDownloads: N/A",
+                    "FTB Revelation\nCreated By: tfox83\nPlays: N/A\nDownloads: N/A",
+                    "Pixelmon Reforged\nPlays: N/A\nDownloads: N/A",
+                    "DirtCraft's DirtBOT\nCreated By: TechDweebGaming & juliann");
+
+            listView.applyCss();
+
             listView.getItems().addAll(list);
+            //listView.getItems().addAll(list);
             hasClicked = true;
         }
 
--- src/main/java/net/dirtcraft/dirtlauncher/Main.java
+++ src/main/java/net/dirtcraft/dirtlauncher/Main.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.net.URL;
 
 public class Main extends Application {
+
+    public static URL url;
 
     public static void main(String[] args) {
         launch(args);
@@ -19,15 +23,23 @@ public class Main extends Application {
 
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+        url = getClass().getResource("resources/backgroundimage.png");
         Parent root = FXMLLoader.load(getClass().getResource("resources/sample.fxml"));
 
         primaryStage.setTitle("Dirt Launcher");
 
-
         Scene scene = new Scene(root, screenSize.width, screenSize.height);
+
+
 
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static ListView setListView(ListView listView) {
+
+
+        return listView;
     }
 
 }
