f315fb643626e9ab11e2720c36338a08d1a3bdb5
= <ShinyTourettesX@gmail.com>
2019-07-09 12:42:25 +0800

    .

--- src/main/java/net/dirtcraft/dirtlauncher/PackCellFactory.java
+++ src/main/java/net/dirtcraft/dirtlauncher/PackCellFactory.java
@@ -11,25 +11,25 @@ import net.dirtcraft.dirtlauncher.backend.JsonUtils.Pack;
 
 public class PackCellFactory extends ListCell<Pack> {
 
-    private PackCellFactory() {    }
+    public PackCellFactory() {    }
 
     protected void updateItem(Pack pack, boolean empty) {
         super.updateItem(pack, empty);
-        String name = pack.getName();
-        String version = pack.getVersion();
 
         if (empty || pack.getName() == null) {
             setText(null);
         } else {
+            String name = pack.getName();
+            String version = pack.getVersion();
             setStyle("-fx-background-color: firebrick; -fx-padding: 1px; -fx-background-insets: 1px;");
             setText(name);
             setAlignment(Pos.CENTER);
             setTextAlignment(TextAlignment.CENTER);
             setTextFill(Paint.valueOf("WHITE"));
             setFont(Font.font("System Bold", FontWeight.LIGHT, 20));
+            setOnMouseClicked(useless->System.out.println(name));
+            setTooltip(new Tooltip(version));
         }
-        setOnMouseClicked(useless->System.out.println(name));
-        setTooltip(new Tooltip(version));
     }
 
 }
