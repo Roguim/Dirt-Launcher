549565c7e29c16092b19d56ad8c664b23eff2af1
= <ShinyTourettesX@gmail.com>
2019-07-09 12:39:35 +0800

    .

--- src/main/java/net/dirtcraft/dirtlauncher/PackCellFactory.java
+++ src/main/java/net/dirtcraft/dirtlauncher/PackCellFactory.java
@@ -11,23 +11,25 @@ import net.dirtcraft.dirtlauncher.backend.JsonUtils.Pack;
 
 public class PackCellFactory extends ListCell<Pack> {
 
-    public PackCellFactory() {    }
+    private PackCellFactory() {    }
 
     protected void updateItem(Pack pack, boolean empty) {
         super.updateItem(pack, empty);
+        String name = pack.getName();
+        String version = pack.getVersion();
 
-        if (empty || pack == null || pack.getName() == null) {
+        if (empty || pack.getName() == null) {
             setText(null);
         } else {
-            setStyle("-fx-background-color: firebrick; -fx-padding: 1px;");
-            setText(pack.getName());
+            setStyle("-fx-background-color: firebrick; -fx-padding: 1px; -fx-background-insets: 1px;");
+            setText(name);
             setAlignment(Pos.CENTER);
             setTextAlignment(TextAlignment.CENTER);
             setTextFill(Paint.valueOf("WHITE"));
             setFont(Font.font("System Bold", FontWeight.LIGHT, 20));
         }
-        setOnMouseClicked(useless->System.out.println(pack.getName()));
-        setTooltip(new Tooltip("Live is short, make most of it..!"));
+        setOnMouseClicked(useless->System.out.println(name));
+        setTooltip(new Tooltip(version));
     }
 
 }
