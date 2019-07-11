89954a78f66518a8757c9381866ce43b83ccb168
TechDweebGaming <jakob.conner42@gmail.com>
2019-07-11 15:21:11 -0700

    Install Game Jar + Assets Final Version
    YAY

--- src/main/java/net/dirtcraft/dirtlauncher/backend/installation/DownloadManager.java
+++ src/main/java/net/dirtcraft/dirtlauncher/backend/installation/DownloadManager.java
@@ -1,20 +1,51 @@
 package net.dirtcraft.dirtlauncher.backend.installation;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import javafx.scene.text.Text;
 import net.dirtcraft.dirtlauncher.Controllers.Install;
+import net.dirtcraft.dirtlauncher.backend.config.Paths;
+import net.dirtcraft.dirtlauncher.backend.jsonutils.JsonFetcher;
 import net.dirtcraft.dirtlauncher.backend.jsonutils.OptionalMod;
 import net.dirtcraft.dirtlauncher.backend.jsonutils.Pack;
+import net.dirtcraft.dirtlauncher.backend.utils.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class DownloadManager {
 
-    public static void completePackSetup(Pack pack, List<OptionalMod> optionalMods) {
+    public static void completePackSetup(Pack pack, List<OptionalMod> optionalMods) throws IOException  {
+        JsonObject versionManifest = JsonFetcher.getVersionManifestJson(pack.getGameVersion());
+
         boolean installMinecraft = true;
         boolean installAssets = true;
-        boolean installLibraries = true;
         boolean installForge = true;
         boolean installPack = true;
+
+        for(JsonElement jsonElement : FileUtils.parseJsonFromFile(Paths.getDirectoryManifest(Paths.getVersionsDirectory())).getAsJsonArray("versions")) {
+            if(jsonElement.getAsJsonObject().get("version").getAsString().equals(pack.getVersion())) installMinecraft = false;
+        }
+        for(JsonElement jsonElement : FileUtils.parseJsonFromFile(Paths.getDirectoryManifest(Paths.getAssetsDirectory())).getAsJsonArray("assets")) {
+            if(jsonElement.getAsJsonObject().get("version").getAsString().equals(versionManifest.get("assets").getAsString())) installAssets = false;
+        }
+        for(JsonElement jsonElement : FileUtils.parseJsonFromFile(Paths.getDirectoryManifest(Paths.getForgeDirectory())).getAsJsonArray("forgeVersions")) {
+            if(jsonElement.getAsJsonObject().get("version").getAsString().equals(pack.getForgeVersion())) installForge = false;
+        }
+        for(JsonElement jsonElement : FileUtils.parseJsonFromFile(Paths.getDirectoryManifest(Paths.getInstancesDirectory())).getAsJsonArray("packs")) {
+            if(jsonElement.getAsJsonObject().get("name").getAsString().equals(pack.getName()) && jsonElement.getAsJsonObject().get("version").getAsString().equals(pack.getVersion())) installPack = false;
+        }
+
+        int totalSteps = optionalMods.size();
+        int completedSteps = 0;
+        if(installMinecraft) totalSteps += 2;
+        if(installAssets) totalSteps++;
+        if(installForge) totalSteps ++;
+        if(installPack) totalSteps++;
+        setTotalProgressPercent(completedSteps, totalSteps);
     }
 
     public static void setProgressText(String text) {
@@ -27,6 +58,108 @@ public class DownloadManager {
 
     public static void setTotalProgressPercent(int completed, int total) {
         Install.getInstance().getBottomBar().setProgress(Math.ceil(completed / total));
+    }
+
+    public static String installMinecraft(JsonObject versionManifest, int completedSteps, int totalSteps) throws IOException {
+        setProgressText("Installing Minecraft " + versionManifest.get("id").getAsString());
+        File versionFolder = new File(Paths.getVersionsDirectory() + File.separator + versionManifest.get("id").getAsString());
+        FileUtils.deleteDirectory(versionFolder);
+        versionFolder.mkdirs();
+
+        // Write version JSON manifest
+        FileUtils.writeJsonToFile(new File(versionFolder.getPath() + File.separator + versionManifest.get("id").getAsString() + ".json"), versionManifest);
+        setProgressPercent(1, 2);
+
+        // Download jar
+        FileUtils.copyURLToFile(versionManifest.getAsJsonObject("downloads").getAsJsonObject("client").get("url").getAsString(), new File(versionFolder.getPath() + File.separator + versionManifest.get("id").getAsString() + ".jar"));
+        setTotalProgressPercent(completedSteps + 1, totalSteps);
+
+        // Download Libraries
+        setProgressText("Downloading Libraries");
+        int completedLibraries = 0;
+        int totalLibraries = versionManifest.getAsJsonArray("libraries").size();
+        setProgressPercent(completedLibraries, totalLibraries);
+        File librariesFolder = new File(versionFolder.getPath() + File.separator + "libraries");
+        librariesFolder.mkdirs();
+        File nativesFolder = new File(versionFolder.getPath() + File.separator + "natives");
+        nativesFolder.mkdirs();
+        String librarysLaunchCode = "";
+
+        libraryLoop:
+        for(JsonElement libraryElement : versionManifest.getAsJsonArray("libraries")) {
+            JsonObject library = libraryElement.getAsJsonObject();
+            // Check if the library has conditions
+            if(library.has("rules")) {
+                for(JsonElement rule : library.getAsJsonArray("rules")) {
+                    switch(rule.getAsJsonObject().get("action").getAsString()) {
+                        case "allow":
+                            if(!rule.getAsJsonObject().has("os")) break;
+                            switch(rule.getAsJsonObject().getAsJsonObject("os").get("name").getAsString()) {
+                                case "windows":
+                                    if(!SystemUtils.IS_OS_WINDOWS) {
+                                        completedLibraries++;
+                                        continue libraryLoop;
+                                    }
+                                case "osx":
+                                    if(!SystemUtils.IS_OS_MAC_OSX) {
+                                        completedLibraries++;
+                                        continue libraryLoop;
+                                    }
+                                case "linux":
+                                    if(!SystemUtils.IS_OS_LINUX) {
+                                        completedLibraries++;
+                                        continue libraryLoop;
+                                    }
+                            }
+                            break;
+                        case "disallow":
+                            if(!rule.getAsJsonObject().has("os")) break;
+                            switch(rule.getAsJsonObject().getAsJsonObject("os").get("name").getAsString()) {
+                                case "windows":
+                                    if(SystemUtils.IS_OS_WINDOWS) {
+                                        completedLibraries++;
+                                        continue libraryLoop;
+                                    }
+                                case "osx":
+                                    if(SystemUtils.IS_OS_MAC_OSX) {
+                                        completedLibraries++;
+                                        continue libraryLoop;
+                                    }
+                                case "linux":
+                                    if(SystemUtils.IS_OS_LINUX) {
+                                        completedLibraries++;
+                                        continue libraryLoop;
+                                    }
+                            }
+                    }
+                }
+            }
+            // The library is not conditional. Continue with the download.
+            JsonObject libraryDownloads = library.getAsJsonObject("downloads");
+            // Download any standard libraries
+            if(libraryDownloads.has("artifact")) {
+                new File(librariesFolder + File.separator + StringUtils.substringBeforeLast(libraryDownloads.getAsJsonObject("artifact").get("path").getAsString(), "/").replace("/", File.separator)).mkdirs();
+                String filePath = librariesFolder.getPath() + File.separator + libraryDownloads.getAsJsonObject("artifact").get("path").getAsString().replace("/", File.separator);
+                FileUtils.copyURLToFile(libraryDownloads.getAsJsonObject("artifact").get("url").getAsString(), new File(filePath));
+            }
+            // Download any natives
+            if(libraryDownloads.has("classifiers")) {
+                String nativesType = "";
+                if(SystemUtils.IS_OS_WINDOWS) nativesType = "natives-windows";
+                if(SystemUtils.IS_OS_MAC_OSX) nativesType = "natives-osx";
+                if(SystemUtils.IS_OS_LINUX) nativesType = "natives-linux";
+                if(libraryDownloads.getAsJsonObject("classifiers").has(nativesType)) {
+                    JsonObject nativeJson = libraryDownloads.getAsJsonObject("classifiers").getAsJsonObject(nativesType);
+                    File outputFile = new File(nativesFolder + File.separator + nativeJson.get("sha1").getAsString());
+                    FileUtils.copyURLToFile(nativeJson.get("url").getAsString(), outputFile);
+                    FileUtils.extractJar(outputFile.getPath(), nativesFolder.getPath());
+                    outputFile.delete();
+                }
+            }
+            completedLibraries++;
+            setProgressPercent(completedLibraries, totalLibraries);
+        }
+        return librarysLaunchCode;
     }
 
 }
--- src/main/java/net/dirtcraft/dirtlauncher/backend/utils/FileUtils.java
+++ src/main/java/net/dirtcraft/dirtlauncher/backend/utils/FileUtils.java
@@ -5,8 +5,12 @@ import com.google.gson.JsonParser;
 
 import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 import java.io.*;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class FileUtils {
 
@@ -45,4 +49,33 @@ public class FileUtils {
         }
     }
 
+    // Mirrors just in case the file imports this instead of the commons-io FileUtils. Makes things easier than using paths every declaration...
+    public static void deleteDirectory(File file) throws IOException {
+        org.apache.commons.io.FileUtils.deleteDirectory(file);
+    }
+
+    public static void copyURLToFile(String URL, File file) throws IOException {
+        org.apache.commons.io.FileUtils.copyURLToFile(new URL(URL), file);
+    }
+
+    public static void extractJar(String jarFile, String destDir) throws IOException {
+        JarFile jar = new JarFile(jarFile);
+        Enumeration enumEntries = jar.entries();
+        while (enumEntries.hasMoreElements()) {
+            JarEntry file = (JarEntry) enumEntries.nextElement();
+            File f = new File(destDir + File.separator + file.getName());
+            if (file.isDirectory()) { // if its a directory, create it
+                f.mkdir();
+                continue;
+            }
+            InputStream is = jar.getInputStream(file); // get the input stream
+            FileOutputStream fos = new FileOutputStream(f);
+            while (is.available() > 0) {  // write contents of 'is' to 'fos'
+                fos.write(is.read());
+            }
+            fos.close();
+            is.close();
+        }
+        jar.close();
+    }
 }
