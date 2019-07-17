653b8e5862a7faadbce33f56088802057ad16337
d4julian <julianubico@icloud.com>
2019-07-17 00:23:47 -0400

    smaller icon for windows
    Took 5 minutes

--- src/main/java/net/dirtcraft/dirtlauncher/backend/components/SystemTray.java
+++ src/main/java/net/dirtcraft/dirtlauncher/backend/components/SystemTray.java
@@ -35,7 +35,7 @@ public class SystemTray {
                 return;
             }
 
-            Image trayImage = ImageIO.read(MiscUtils.getResourceStream(Internal.ICONS, "dirticon_small.png"));
+            Image trayImage = ImageIO.read(MiscUtils.getResourceStream(Internal.ICONS, SystemUtils.IS_OS_WINDOWS ? "dirticon_tiny.png" : "dirticon_small.png"));
 
             // set up a system tray icon.
 
